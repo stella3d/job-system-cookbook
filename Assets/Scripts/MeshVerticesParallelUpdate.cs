61ad19ef62d44018aee794d947f8407954bd62e6
Stella Cannefax <stella@unity3d.com>
2018-01-29 23:41:34 -0800

    mesh modification example works, in the best way

---
+++ Assets/Scripts/MeshVerticesParallelUpdate.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using Unity.Collections;
+using Unity.Jobs;
+
+public class MeshVerticesParallelUpdate : MonoBehaviour
+{
+    public int vertexCount = 20000;
+
+    NativeArray<Vector3> m_Vertices;
+    NativeArray<Vector3> m_Normals;
+    NativeArray<Vector2> m_Uv;
+    NativeArray<int> m_Triangles;
+
+    CalculateJob m_CalculateJob;
+
+    JobHandle m_PositionJobHandle;
+    JobHandle m_JobHandle;
+
+    MeshFilter m_MeshFilter;
+    Mesh m_Mesh;
+
+    protected void Start()
+    {
+        m_MeshFilter = gameObject.GetComponent<MeshFilter>();
+        m_Mesh = m_MeshFilter.mesh;
+        m_Mesh.MarkDynamic();
+        
+        // this persistent memory setup assumes our vertex count will not expand
+        m_Vertices = new NativeArray<Vector3>(m_Mesh.vertices, Allocator.Persistent);
+        m_Normals = new NativeArray<Vector3>(m_Mesh.normals, Allocator.Persistent);
+        m_Uv = new NativeArray<Vector2>(m_Mesh.uv, Allocator.Persistent);
+        m_Triangles = new NativeArray<int>(m_Mesh.triangles, Allocator.Persistent);
+    }
+
+    struct CalculateJob : IJobParallelFor
+    {
+        public NativeArray<Vector3> vertices;
+
+        //public NativeArray<Vector2> uv;
+
+        // public NativeArray<int> triangles;
+
+        public float time;
+
+        public void Execute(int i)
+        {
+            var vertex = vertices[i];
+            var noise = Vector3.one * Mathf.PerlinNoise(vertex.z, vertex.y);
+            var sine = Vector3.one * Mathf.Sin(time);
+
+            vertex = vertex - sine + noise;
+
+            vertices[i] = vertex;
+            //uv[i] = new Vector2(vertex.x, vertex.z);
+        }
+    }
+
+    public void LateUpdate()
+    {
+        m_JobHandle.Complete();
+
+        m_Mesh.vertices = m_CalculateJob.vertices.ToArray();
+        //m_Mesh.uv = m_CalculateJob.uv.ToArray();
+    }
+
+    public void Update()
+    {
+        m_CalculateJob = new CalculateJob()
+        {
+            vertices = m_Vertices,
+            // uv = m_Uv,
+            time = Time.time
+        };
+
+        m_JobHandle = m_CalculateJob.Schedule(m_Vertices.Length, 64);
+    }
+
+    private void OnDestroy()
+    {
+        m_Vertices.Dispose();
+        m_Normals.Dispose();
+        m_Uv.Dispose();
+        m_Triangles.Dispose();
+    }
+}
--- Assets/Scripts/SetupUtils.cs
+++ Assets/Scripts/SetupUtils.cs
@@ -4,26 +4,6 @@ using UnityEngine;
 
 public static class SetupUtils
 {
-    public static GameObject[] Place2DCubeGrid(int sideLength)
-    {
-        var count = sideLength * sideLength;
-        var cubes = new List<GameObject>(count);
-
-        var cubeToCopy = MakeStrippedCube();
-
-        for (int i = 0; i < sideLength; i++)
-        {
-            for (int n = 0; n < sideLength; n++)
-            {
-                var cube = GameObject.Instantiate(cubeToCopy);
-                cube.transform.position = new Vector3(i, n);
-                cubes.Add(cube);
-            }
-        }
-
-        return cubes.ToArray();
-    }
-
     public static GameObject[] PlaceRandomCubes(int count, float radius)
     {
         var cubes = new GameObject[count];

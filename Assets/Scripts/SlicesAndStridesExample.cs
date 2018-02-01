ddc91e850de5f3a51eb7b75e33c1a37bf151a937
Stella Cannefax <stella@unity3d.com>
2018-02-01 03:29:25 -0800

    clean up strides example

--- Assets/Scripts/SlicesAndStridesExample.cs
+++ Assets/Scripts/SlicesAndStridesExample.cs
@@ -5,44 +5,25 @@ using Unity.Collections;
 
 public class SlicesAndStridesExample : MonoBehaviour
 {
-    NativeArray<Vector4> m_PointCloud;
+    [SerializeField]
+    protected int m_PointCount = 10000;
 
+    NativeArray<Vector4> m_PointCloud;
     NativeArray<float> m_Distances;
 
-    ConfidenceAverageSliceJob m_ConfidenceProcessingJob;
-    ChangePoints m_ChangePointsJob;
-
+    UpdatePointCloudJob m_UpdatePointCloudJob;
+    ConfidenceProcessingJob m_ConfidenceProcessingJob;
     DistanceParallelJob m_DistanceParallelJob;
-    AverageHorizontalDistanceJob m_AverageGroundDistanceJob;
+    AverageGroundDistanceJob m_AverageGroundDistanceJob;
 
     JobHandle m_ParallelDistanceJobHandle;
     JobHandle m_DistanceJobHandle;
-
     JobHandle m_ConfidenceJobHandle;
-    JobHandle m_ChangeJobHandle;
+    JobHandle m_PointCloudUpdateHandle;
 
-    float m_AverageConfidence;
+    static int updateCount;
 
-    protected void Start()
-    {
-        // this persistent memory setup assumes our vertex count will not expand
-        m_PointCloud = new NativeArray<Vector4>(10000, Allocator.Persistent);
-        m_Distances = new NativeArray<float>(10000, Allocator.Persistent);
-
-        for (int i = 0; i < m_PointCloud.Length; i++)
-        {
-            m_PointCloud[i] = RandomVec4();
-        }
-    }
-
-    Vector4 RandomVec4()
-    {
-        var vec3 = UnityEngine.Random.insideUnitSphere;
-        var w = UnityEngine.Random.value;
-        return new Vector4(vec3.x, vec3.y, vec3.z, w);
-    }
-
-    struct ChangePoints : IJobParallelFor
+    struct UpdatePointCloudJob : IJobParallelFor
     {
         [WriteOnly]
         public NativeArray<Vector4> points;
@@ -61,6 +42,7 @@ public class SlicesAndStridesExample : MonoBehaviour
 
         public void Execute(int i)
         {
+            // semi-randomly skip updating points - we care about averages here
             if (i % 2 == 0 && sinTimeRandom < 0.3f || sinTimeRandom > 0.9f)
                 return;
 
@@ -72,7 +54,7 @@ public class SlicesAndStridesExample : MonoBehaviour
         }
     }
 
-    struct ConfidenceAverageSliceJob : IJob
+    struct ConfidenceProcessingJob : IJob
     {
         [ReadOnly]
         public NativeSlice<float> confidence;
@@ -94,38 +76,8 @@ public class SlicesAndStridesExample : MonoBehaviour
         }
     }
 
-    struct AverageHorizontalDistanceJob : IJob
-    {
-        [ReadOnly]
-        public NativeArray<float> distances;
 
-        [WriteOnly]
-        public NativeArray<float> average;
-
-        public void Execute()
-        {
-            float sum = 0f;
-            float lowest = Single.MaxValue;
-            float highest = Single.MinValue;
-
-            for (int i = 0; i < distances.Length; i++)
-            {
-                var dist = distances[i];
-
-                if (dist < lowest)
-                    lowest = dist;
-                if (dist > highest)
-                    highest = dist;
-
-                sum += dist;
-            }
-
-            average[0] = sum / distances.Length;
-            average[1] = lowest;   
-            average[2] = highest;
-        }
-    }
-
+    // calculate the horizontal distance of every point in parallel
     struct DistanceParallelJob : IJobParallelFor
     {
         [ReadOnly]
@@ -149,37 +101,63 @@ public class SlicesAndStridesExample : MonoBehaviour
         }
     }
 
-    struct AverageHeightJob : IJob
+    // once we've calculated distances, find mix, max, & average,
+    // using a non-parallel job.  You could also do this in "buckets"
+    // using another parallel job if your dataset was very large -
+    // find the local min / max / sum for each bucket, then do this
+    struct AverageGroundDistanceJob : IJob
     {
         [ReadOnly]
-        public NativeSlice<float> y;
-
-        [ReadOnly]
-        public float compareValue;
+        public NativeArray<float> distances;
 
         [WriteOnly]
         public NativeArray<float> average;
 
         public void Execute()
         {
-            float sumY = 0f;
-            for (int i = 0; i < y.Length; i++)
-                sumY += Mathf.Abs(compareValue - y[i]);
+            float sum = 0f;
+            float lowest = Single.MaxValue;
+            float highest = Single.MinValue;
 
-            average[0] = sumY / y.Length;
+            for (int i = 0; i < distances.Length; i++)
+            {
+                var dist = distances[i];
+                if (dist < lowest)
+                    lowest = dist;
+                if (dist > highest)
+                    highest = dist;
+
+                sum += dist;
+            }
+
+            average[0] = sum / distances.Length;
+            average[1] = lowest;
+            average[2] = highest;
         }
+    }
+
+    protected void Start()
+    {
+        // this persistent memory setup assumes our point count will not expand
+        m_PointCloud = new NativeArray<Vector4>(m_PointCount, Allocator.Persistent);
+        m_Distances = new NativeArray<float>(m_PointCount, Allocator.Persistent);
+
+        for (int i = 0; i < m_PointCloud.Length; i++)
+            m_PointCloud[i] = RandomVec4();
     }
 
     public void Update()
     {
         var slice = new NativeSlice<Vector4>(m_PointCloud, 0);
 
-        m_ConfidenceProcessingJob = new ConfidenceAverageSliceJob()
+        m_ConfidenceProcessingJob = new ConfidenceProcessingJob()
         {
-            // sample every other confidence point
+            // 2 = sample every other confidence point.  this value exists
+            // to tune accuracy vs speed here, is not part of the job system
             sampleStride = 2,
+
             // this stride is all the "w" values of the vectors
-            confidence = slice.SliceWithStride<float>(12),  
+            confidence = slice.SliceWithStride<float>(12),
             average = new NativeArray<float>(1, Allocator.TempJob),
         };
 
@@ -191,33 +169,62 @@ public class SlicesAndStridesExample : MonoBehaviour
             distances = m_Distances
         };
 
-        m_AverageGroundDistanceJob = new AverageHorizontalDistanceJob()
+        m_AverageGroundDistanceJob = new AverageGroundDistanceJob()
         {
             distances = m_Distances,
             average = new NativeArray<float>(3, Allocator.TempJob)
         };
 
-        m_ConfidenceJobHandle = m_ConfidenceProcessingJob.Schedule(m_ChangeJobHandle);
+        m_ConfidenceJobHandle = m_ConfidenceProcessingJob.Schedule(m_PointCloudUpdateHandle);
 
         m_ParallelDistanceJobHandle = m_DistanceParallelJob
-            .Schedule(m_Distances.Length, 128, m_ChangeJobHandle);
+            .Schedule(m_Distances.Length, 128, m_PointCloudUpdateHandle);
 
         m_DistanceJobHandle = m_AverageGroundDistanceJob.Schedule(m_ParallelDistanceJobHandle);
 
         updateCount++;
     }
 
-    static int updateCount;
-
     public void LateUpdate()
     {
+        // make sure both job chains we started in Update complete
         m_ConfidenceJobHandle.Complete();
         m_DistanceJobHandle.Complete();
 
+        PrintDebugInfo();
+
+        // dispose any temporary job allocations
+        m_ConfidenceProcessingJob.average.Dispose();
+        m_AverageGroundDistanceJob.average.Dispose();
+
+        // change our point cloud ahead of the next frame
+        ScheduleNextPointUpdateJob();
+    }
+
+    void ScheduleNextPointUpdateJob()
+    {
+        // change some of the points before next frame to simulate pointcloud
+        var turbulence = Mathf.Clamp01(UnityEngine.Random.value + 0.1f);
+
+        m_UpdatePointCloudJob = new UpdatePointCloudJob()
+        {
+            points = m_PointCloud,
+            sinTimeRandom = Mathf.Sin(Time.time) * turbulence,
+            cosTimeRandom = Mathf.Cos(Time.time) * turbulence,
+            random = turbulence,
+            frameCount = Time.frameCount
+        };
+
+        m_PointCloudUpdateHandle = m_UpdatePointCloudJob.Schedule(m_PointCloud.Length, 64);
+    }
+
+    void PrintDebugInfo()
+    {
         // this is just to only log info every n updates
-        if (updateCount % 120 == 0 || updateCount == 1)
+        if (updateCount % 150 == 0 || updateCount == 1)
         {
             var distances = m_AverageGroundDistanceJob.average;
+
             var groundDistance = distances[0];
             var distanceMin = distances[1];
             var distanceMax = distances[2];
@@ -228,34 +235,31 @@ public class SlicesAndStridesExample : MonoBehaviour
             var conf = m_ConfidenceProcessingJob.average[0];
             Debug.Log("confidence average: " + conf);
         }
+    }
 
-        // dispose any temporary job allocations
-        m_ConfidenceProcessingJob.average.Dispose();
-        m_AverageGroundDistanceJob.average.Dispose();
-
-
-        // change some of the points before next frame to simulate pointcloud
-        var changeNoise = Mathf.Clamp01(UnityEngine.Random.value + 0.1f);
-
-        m_ChangePointsJob = new ChangePoints()
-        {
-            points = m_PointCloud,
-            sinTimeRandom = Mathf.Sin(Time.time) * changeNoise,
-            cosTimeRandom = Mathf.Cos(Time.time) * changeNoise,
-            random = changeNoise,
-            frameCount = Time.frameCount
-        };
-
-        m_ChangeJobHandle = m_ChangePointsJob.Schedule(m_PointCloud.Length, 64);
+    Vector4 RandomVec4()
+    {
+        var vec3 = UnityEngine.Random.insideUnitSphere;
+        var w = UnityEngine.Random.value;
+        return new Vector4(vec3.x, vec3.y, vec3.z, w);
     }
 
     private void OnDestroy()
     {
+        // handle temp allocations in case OnDestroy happens in a weird spot
+        if (m_ConfidenceProcessingJob.average.IsCreated)
+        {
+            m_ConfidenceProcessingJob.average.Dispose();
+            m_AverageGroundDistanceJob.average.Dispose();
+        }
+
+        // make sure we don't have running jobs
+        if (!m_PointCloudUpdateHandle.IsCompleted)
+            m_PointCloudUpdateHandle.Complete();
+
+        // dispose our permanent allocations
         m_PointCloud.Dispose();
+        m_Distances.Dispose();
     }
 
-    private void OnPlayModeExit()
-    {
-        m_PointCloud.Dispose();
-    }
 }
